379f4041efe5496a8bbe97359d327b01ba167433
Hrishikesh Yadav <hrishikeshyadav@Hrishikeshs-MacBook-Air.local>
2025-04-08 22:00:17 +0530

    added section-3

---
+++ section-3/starting-project-3/screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, TextInput } from 'react-native';
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput>{/* buttons */}</TextInput>
+    </View>
+  );
+}
+
+export default StartGameScreen;
